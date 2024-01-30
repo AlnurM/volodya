594c93e8a46237df0b065545f002bf9e11bb97cf
AlnurM <alnur.mustafayev@gmail.com>
2024-01-30 20:31:55 +0600

    fix: getUser

--- src/pageComponents/HomePage/HomePage.jsx
+++ src/pageComponents/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 import { useTelegram } from '@/hooks'
 import { db } from '@/firebase'
@@ -6,7 +6,18 @@ import styles from './index.module.sass'
 
 const HomePage = () => {
   const { tg, user } = useTelegram()
+  const [currentUser, setCurrentUser] = useState(null)
 
+  const getUser = async () => {
+    if (!user?.id) {
+      return 
+    }
+    const docSnap = await getDoc(doc(db, 'user', user.id.toString()))
+    if (docSnap.exists()) {
+      return setCurrentUser(docSnap.data())
+    }
+  }
+  console.log(currentUser)
   const handleRegister = async () => {
     if (!user?.id) {
       return 
@@ -17,10 +28,7 @@ const HomePage = () => {
   }
 
   useEffect(() => {
-    if (!user?.id) {
-      return 
-    }
-    getDoc(doc(db, 'user', user.id.toString())).then(res => console.log(res))
+    getUser()
   }, [user?.id])
   return (
     <div className={styles.Root}>
